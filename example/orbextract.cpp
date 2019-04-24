649325b9a906ddcfed63686dd57b083b47c717c5
luzhaozhang2276 <luzhaozhang@mail.dlut.edu.cn>
2019-04-24 15:30:13 +0800

    删除临时变量

--- example/orbextract.cpp
+++ example/orbextract.cpp
@@ -22,7 +22,7 @@ using namespace std;
 using namespace cv;
 using namespace ORB_SLAM2;
 
-#define d_max_vaule 14
+#define d_max_vaule 40
 
 int main()
 {
@@ -36,6 +36,7 @@ int main()
     int fMinThFAST =8;          // 8
 
     int level = 0;
+    int temp=0;
     /**************** 图片一：初始化信息 *********************/
     cv::Mat first_image = cv::imread(file1, 0);    // load grayscale image 灰度图
     cv::Mat feature1;
@@ -52,46 +53,41 @@ int main()
     orb1->myprint("picture1");
     mvImageShow1 = orb1->GetImagePyramid();   //获取图像金字塔
 
-
-
     mnFeaturesPerLevel1 = orb1->GetmnFeaturesPerLevel();  //获取每层金字塔的特征点数量
 
-    int i=level;
     mvvKeypoints1 = orb1->GetmvvKeypoints();
-    mvKeys1 = mvvKeypoints1[i];
-    mDes1 = mDescriptors1.rowRange(0,mnFeaturesPerLevel1[i]).clone();
+    mvKeys1 = mvvKeypoints1[level];
+    mDes1 = mDescriptors1.rowRange(0,mnFeaturesPerLevel1[level]).clone();
 
-    cout <<"KeyPoints:"<<mnFeaturesPerLevel1[i]<<endl;
-    cv::drawKeypoints(mvImageShow1[i], mvKeys1, feature1, cv::Scalar::all(-1),
+    cout <<"KeyPoints:"<<mnFeaturesPerLevel1[level]<<endl;
+    cv::drawKeypoints(mvImageShow1[level], mvKeys1, feature1, cv::Scalar::all(-1),
                       cv::DrawMatchesFlags::DEFAULT);//DEFAULT  DRAW_OVER_OUTIMG     DRAW_RICH_KEYPOINTS
     ///delaunay
-    std::vector<Vector2<float> > points;
-    int index1=0;
-    for(const auto &k :mvKeys1)
-    {
-//        if(index1==70)
-//            cout << index1  << " , " << k.pt.x <<" , "<< k.pt.y<< endl;
-
-        points.emplace_back(Vector2<float>(k.pt.x,k.pt.y,index1));
-        index1++;
-//        cout << k.class_id<< endl;
-    }
-    cout << "Size of points1: " << points.size() << endl;
-
-    Delaunay<float> triangulation;
-    const std::vector<Triangle<float> > triangles = triangulation.triangulate(points);  //逐点插入法
-    std::cout << triangles.size() << " triangles generated\n";
-    const std::vector<Edge<float> > edges = triangulation.getEdges();
-    std::cout << "Edges : " << edges.size() << std::endl;
-
-    for(const auto &e : edges)
-    {
-        line(feature1, Point(e.p1.x, e.p1.y), Point(e.p2.x, e.p2.y), Scalar(0, 0, 255), 1);
-    }
-
-    imwrite("feature1.png",feature1);
-    imshow("feature1",feature1);
-    waitKey(0);
+//    std::vector<Vector2<float> > points;
+//    temp = 0;
+//    for(const auto &k :mvKeys1)
+//    {
+//        points.emplace_back(Vector2<float>(k.pt.x,k.pt.y,temp));
+//        temp++;
+//    }
+//    temp = 0;
+//
+//    cout << "Size of points1: " << points.size() << endl;
+//
+//    Delaunay<float> triangulation;
+//    const std::vector<Triangle<float> > triangles = triangulation.triangulate(points);  //逐点插入法
+//    std::cout << triangles.size() << " triangles generated\n";
+//    const std::vector<Edge<float> > edges = triangulation.getEdges();
+//    std::cout << "Edges : " << edges.size() << std::endl;
+//
+//    for(const auto &e : edges)
+//    {
+//        line(feature1, Point(e.p1.x, e.p1.y), Point(e.p2.x, e.p2.y), Scalar(0, 0, 255), 1);
+//    }
+//
+//    imwrite("feature1.png",feature1);
+//    imshow("feature1",feature1);
+//    waitKey(0);
     /**************** 图片二：初始化信息 *********************/
     cv::Mat second_image = cv::imread(file2, 0);    // load grayscale image 灰度图
     cv::Mat feature2;
@@ -108,46 +104,41 @@ int main()
     orb2->myprint("picture2");
     mvImageShow2 = orb2->GetImagePyramid();   //获取图像金字塔
 
-
-
     mnFeaturesPerLevel2 = orb2->GetmnFeaturesPerLevel();  //获取每层金字塔的特征点数量
 
-    int j=level;
     mvvKeypoints2 = orb2->GetmvvKeypoints();
-    mvKeys2 = mvvKeypoints2[j];
-    mDes2 = mDescriptors2.rowRange(0,mnFeaturesPerLevel2[j]).clone();
+    mvKeys2 = mvvKeypoints2[level];
+    mDes2 = mDescriptors2.rowRange(0,mnFeaturesPerLevel2[level]).clone();
 
-    cout <<"KeyPoints:"<<mnFeaturesPerLevel2[j]<<endl;
-    cv::drawKeypoints(mvImageShow2[j], mvKeys2, feature2, cv::Scalar::all(-1),
+    cout <<"KeyPoints:"<<mnFeaturesPerLevel2[level]<<endl;
+    cv::drawKeypoints(mvImageShow2[level], mvKeys2, feature2, cv::Scalar::all(-1),
                       cv::DrawMatchesFlags::DEFAULT);//DEFAULT  DRAW_OVER_OUTIMG     DRAW_RICH_KEYPOINTS
 
     ///delaunay
-    std::vector<Vector2<float> > points2;
-    int index2=0;
-    for(const auto &k2 :mvKeys2)
-    {
-//        if(index2==76)
-//            cout << index2  << " , " << k2.pt.x <<" , "<< k2.pt.y<< endl;
-
-
-        points2.emplace_back(Vector2<float>(k2.pt.x,k2.pt.y,index2));
-        index2++;
-    }
+//    std::vector<Vector2<float> > points2;
+//    temp = 0;
+//    for(const auto &k2 :mvKeys2)
+//    {
+//        points2.emplace_back(Vector2<float>(k2.pt.x,k2.pt.y,temp));
+//        temp++;
+//    }
+//    temp = 0;
+//
 //    cout << "Size of points2: " << points2.size() << endl;
-    Delaunay<float> triangulation2;
-    const std::vector<Triangle<float> > triangles2 = triangulation2.triangulate(points2);  //逐点插入法
-    std::cout << triangles2.size() << " triangles generated\n";
-    const std::vector<Edge<float> > edges2 = triangulation2.getEdges();
+//    Delaunay<float> triangulation2;
+//    const std::vector<Triangle<float> > triangles2 = triangulation2.triangulate(points2);  //逐点插入法
+//    std::cout << triangles2.size() << " triangles generated\n";
+//    const std::vector<Edge<float> > edges2 = triangulation2.getEdges();
+//
+//    for(const auto &e2 : edges2)
+//    {
+//        line(feature2, Point(e2.p1.x, e2.p1.y), Point(e2.p2.x, e2.p2.y), Scalar(0, 0, 255), 1);
+//    }
 
-    for(const auto &e2 : edges2)
-    {
-        line(feature2, Point(e2.p1.x, e2.p1.y), Point(e2.p2.x, e2.p2.y), Scalar(0, 0, 255), 1);
-    }
 
-
-    imwrite("feature2.png",feature2);
-    imshow("feature2",feature2);
-    waitKey(0);
+//    imwrite("feature2.png",feature2);
+//    imshow("feature2",feature2);
+//    waitKey(0);
     /**************** 特征匹配 ******************/
     vector<DMatch> matches,good_matches;
     BFMatcher matcher (NORM_HAMMING);
@@ -167,22 +158,28 @@ int main()
 
     int d_max =d_max_vaule;
     //筛选匹配
+    temp=0;
     for (int l = 0; l < mDes1.rows; l++)
     {
 //        if(matches[l].distance <= max(2*min_dist,30.0) )
         if(matches[l].distance <= d_max_vaule )
-            good_matches.push_back(matches[l]);
+        {
+            matches[l].imgIdx=temp;
+            good_matches.emplace_back(matches[l]);
+            temp++;
+        }
     }
+    temp=0;
 
-//    for(const auto &m:matches)
+//    cerr << "test"<< endl;
+//    for(const auto &m:good_matches)
 //    {
 //        cout << m.queryIdx << " , " << m.trainIdx << " , " << m.imgIdx << endl;
 //    }
     /**************** 显示 ******************/
     cout << "match:" << good_matches.size()<<endl;
-
     Mat show;
-    cv::drawMatches(mvImageShow1[j],mvKeys1,mvImageShow2[j],mvKeys2,good_matches,show);
+    cv::drawMatches(mvImageShow1[level],mvKeys1,mvImageShow2[level],mvKeys2,good_matches,show);
     imwrite("matches.png",show);
     imshow("matches",show);
     waitKey(0);
